c3719a5ce5f6db7bab35ac3d6f6794bedda4312c
pocketcat3 <114851989+pocketcat3@users.noreply.github.com>
2022-10-23 11:45:41 +0300

    Update sketch_oct23a.ino

--- sketch_oct23a.ino
+++ sketch_oct23a.ino
@@ -7,7 +7,7 @@ byte customChar[8] = {
   B01100110,
   B01100110,
   B00000000};
-}
+
 
 #include <LedControl.h>
 LedControl lc=LedControl(12,11,10,1);
@@ -18,12 +18,12 @@ void setup() {
 }
 
 void loop() {
-  lc.setRow(0, i, a[0]);
-  lc.setRow(0, i, a[1]);
-  lc.setRow(0, i, a[2]);
-  lc.setRow(0, i, a[3]);
-  lc.setRow(0, i, a[4]);
-  lc.setRow(0, i, a[5]);
-  lc.setRow(0, i, a[6]);
-  lc.setRow(0, i, a[7]);
+  lc.setRow(0, 0, a[0]);
+  lc.setRow(0, 1, a[1]);
+  lc.setRow(0, 2, a[2]);
+  lc.setRow(0, 3, a[3]);
+  lc.setRow(0, 4, a[4]);
+  lc.setRow(0, 5, a[5]);
+  lc.setRow(0, 6, a[6]);
+  lc.setRow(0, 7, a[7]);
 }
